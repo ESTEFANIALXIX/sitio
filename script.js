067df5564d04d4485a5590e7b3c3730fdfb31927
Estefania Suarez <eestefania.suarez@gmail.com>
2021-09-02 17:11:24 -0500

    nuevo

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlide(slideIndex);
 
+function plusSlides(n){
+    showSlide(slideIndex += n);
+}
+
+function currentSlides(n){
+    showSlide(slideIndex = n);
+    
+}
+
 function showSlide(n){
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
 
     if(n > slides.length){
         slideIndex = 1;
@@ -17,4 +25,17 @@ function showSlide(n){
     for (let i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className.replace(" active", "");   
+    }
+
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+
+    slideIndex++;
+
+    setTimeout(() =>{
+        showSlide(slideIndex);
+    }, 7777);
 }
